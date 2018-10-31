d68314f81a61a9800ed6324538f6f926b264088b
Robson Silva <robsonsilva@gmail.com>
2018-10-31 15:21:47 -0300

    Atualização do Cronograma e alerta no login

--- login.php
+++ login.php
@@ -1,22 +1,30 @@
 <?php
 
-//session_start();
-
-include 'funcoes.php';
-include 'conexao.php';
+include_once ("funcoes.php");
+include_once ("conexao.php");
 
 $usuario = filter_input(INPUT_POST, 'user', FILTER_SANITIZE_STRING);
 $senha = filter_input(INPUT_POST, 'pwd', FILTER_SANITIZE_STRING);
 
 $tipo = validasuario($usuario, $senha, $con);
 
+if (isset($_SESSION['tipoUsuario'])) {
+    unset($_SESSION['tipoUsuario']);
+}
+
 $_SESSION['tipoUsuario'] = $tipo;
 
 if ($tipo == "adm") {
     header('location: paginaInicialAdm.php');
-} else if ($_SESSION['tipoUsuario'] == "secretaria") {
+} else if ($tipo == "secretaria") {
     header('location: secretarias/paginaInicialSec.php');
-} else if ($_SESSION['tipoUsuario'] == "professor") {
-    header('location: paginaInicialProf.php');
+} else if ($tipo == "professor") {
+    header('location: professor/paginaInicialProf.php');
+}
+else
+{
+    $_SESSION['msg'] = "<p><span style = 'color: red'>Usuário inválido</span></p> ";
+    
+     header('location: index.php');
 }
 
