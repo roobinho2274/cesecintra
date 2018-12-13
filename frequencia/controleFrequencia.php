0f02d83929cd437a2e2d00abff5f4989a1e0946e
Robson Silva <robsonsilva@gmail.com>
2018-12-13 10:49:08 -0200

    Mudança da ideia de secretária para atributo de professor

--- frequencia/controleFrequencia.php
+++ frequencia/controleFrequencia.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 ?>
@@ -8,10 +7,6 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<script>
-    function direciona()
-    {}
-</script>
 <html>
     <head>
         <!-- Required meta tags -->
@@ -20,21 +15,24 @@ and open the template in the editor.
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="../css/bootstrap.css" >
-   
+
 
         <title>Pagina Administrador</title>
 
     </head>
     <body>
         <div class = "container" style="background-color: buttonface" >
-            <h2>DISCIPLINAS</h2>
+            <h2>FREQUÊNCIA</h2>
             <form >
-                 <input type="submit" class="btn btn-secondary btn-block" value="CADASTRA DISCIPLINA" formaction="cadastraDis.php"/><br><br>
-                 <input type="submit" class="btn btn-secondary btn-block" value="CONSULTA DISCIPLINA" formaction="listaDis.php"/><br><br>
-                 <input type="submit" class="btn btn-secondary btn-block" value="ALTERA DISCIPLINA" formaction="alteraDis.php"/><br><br>
-                 <input type="submit" class="btn btn-secondary btn-block" value="DELETA DISCIPLINA" formaction="deletaDis.php"/><br><br>
-            </form>
+                <input type="submit" class="btn btn-secondary btn-block" value="LANÇA FREQUÊNCIA" formaction="cadFrequencia.php"/><br><br>
+                <input type="submit" class="btn btn-secondary btn-block" value="CONSULTA FREQUÊNCIA" formaction="listaFrequencia.php"/><br><br>
 
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary" formaction="../paginaInicialAdm.php">Voltar</button>
+                    </div>
+                </div>
+            </form>
             <script src="../js/jquery-3.3.1.slim.min.js"></script>
             <script src="../js/popper.min.js"></script>
             <script src="../js/bootstrap.min.js"></script>
--- paginaInicialAdm.php
+++ paginaInicialAdm.php
@@ -17,15 +17,14 @@ session_start();
     <body>
         <h2> PAGINA ADMINISTRADOR</h2>
         <div class = "container" style="background-color: buttonface" >
-           
+
             <form name="delta" action="delDisc.php">
                 <input type="submit" class="btn btn-primary btn-block" value="Controle Aluno" formaction="aluno/controleAluno.php"/><br><br>
-                <input type="submit" class="btn btn-primary btn-block " value="Controle Professores" formaction="professor/controleProfessor.php"/><br><br>
-                <input type="submit" class="btn btn-primary btn-block" value="Controle Secretarias" formaction="secretarias/controleSecretarias.php"/><br><br>
+                <input type="submit" class="btn btn-primary btn-block " value="Controle Servidor" formaction="professor/controleProfessor.php"/><br><br>
                 <input type="submit" class="btn btn-primary btn-block" value="Controle Disciplinas" formaction="disciplina/controleDisciplinas.php"/><br><br>
                 <input type="submit" class="btn btn-primary btn-block" value="Controle Matriculas" formaction="matriculas/controleMatriculas.php"/><br><br>
-                <input type="submit" class="btn btn-primary btn-block" value="Backup Banco" formaction="../cesecintra/backupBD.php"/><br><br>
-             </form>
+                <input type="submit" class="btn btn-primary btn-block" value="Controle Frequência" formaction="frequencia/controleFrequencia.php"/><br><br>
+            </form>
 
             <script src="js/jquery-3.3.1.slim.min.js"></script>
             <script src="js/popper.min.js"></script>
