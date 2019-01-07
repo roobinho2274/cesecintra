23fbc84cb6c0bc5748e96ba01bed7078f05329cf
jonas braga <jonasbraga2001@gmail.com>
2019-01-07 14:47:50 -0200

    Bd modificado

--- cesecbd.sql
+++ cesecbd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 13-Dez-2018 às 14:07
--- Versão do servidor: 10.1.36-MariaDB
--- versão do PHP: 7.2.10
+-- Generation Time: 07-Jan-2019 às 17:35
+-- Versão do servidor: 10.1.31-MariaDB
+-- versão do PHP: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -33,6 +33,7 @@ CREATE TABLE `aluno` (
   `rg` varchar(10) NOT NULL,
   `cpf` bigint(20) NOT NULL,
   `nome` varchar(100) NOT NULL,
+  `turno` int(11) NOT NULL,
   `orgaoExpedidor` varchar(10) NOT NULL,
   `mae` varchar(100) NOT NULL,
   `pai` varchar(100) DEFAULT NULL,
@@ -59,9 +60,11 @@ CREATE TABLE `aluno` (
 -- Extraindo dados da tabela `aluno`
 --
 
-INSERT INTO `aluno` (`id`, `rg`, `cpf`, `nome`, `orgaoExpedidor`, `mae`, `pai`, `tituloEleitor`, `reservista`, `sexo`, `estadoCivil`, `logradouro`, `bairro`, `complemento`, `numeroResidencial`, `cidade`, `cep`, `estado`, `telefone`, `email`, `celular`, `status`, `datacadastro`, `grauensino`) VALUES
-(1, '1111111111', 1111111111, 'Tobias Pereira dos Santos', 'SSPMG', 'Maria Tobias', 'JoÃ£o Tobias', 1111111111, 0, '', '', '', '', '', 0, '', 0, 'MG', 35991083257, 'robinho2274@gmail.com', 35991083257, '1', '2018-12-13', '1'),
-(2, '222222222', 222222222, 'Jonas Henrique', 'SSMG', 'Laurelise Costa', NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1234567890, 'jonas@gmail.com', 87654321, 'ATIVO', '2018-12-13', '1');
+INSERT INTO `aluno` (`id`, `rg`, `cpf`, `nome`, `turno`, `orgaoExpedidor`, `mae`, `pai`, `tituloEleitor`, `reservista`, `sexo`, `estadoCivil`, `logradouro`, `bairro`, `complemento`, `numeroResidencial`, `cidade`, `cep`, `estado`, `telefone`, `email`, `celular`, `status`, `datacadastro`, `grauensino`) VALUES
+(1, '1111111111', 1111111111, 'Tobias Pereira dos Santos', 1, 'SSPMG', 'Maria Tobias', 'JoÃ£o Tobias', 1111111111, 0, '', '', '', '', '', 0, '', 0, 'MG', 35991083257, 'robinho2274@gmail.com', 35991083257, '1', '2018-12-13', '1'),
+(2, '222222222', 222222222, 'Jonas Henrique', 2, 'SSMG', 'Laurelise Costa', ' ', 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1234567890, 'jonas@gmail.com', 87654321, 'ATIVO', '2018-12-13', '1'),
+(3, '123123123', 123123123, 'twobias', 1, 'SSPMG', 'Maria Tobias', 'JoÃ£o Tobias', 123123123, 0, '', '', '', '', '', 0, '', 0, 'MG', 3599083257, 'robinho274@gmail.com', 3599108257, '1', '2018-12-13', '1'),
+(4, '1121212121', 121212121212, 'joninhas', 1, 'SSMG', 'Laurelise Costa', ' ', 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 12312, 'jonass@gmail.com', 8722234, 'ATIVO', '2018-12-13', '1');
 
 -- --------------------------------------------------------
 
@@ -73,23 +76,25 @@ CREATE TABLE `disciplina` (
   `id` int(11) NOT NULL,
   `descricao` varchar(100) NOT NULL,
   `idGrauEnsino` int(11) NOT NULL,
-  `idProf` int(11) NOT NULL
+  `idProf` int(11) NOT NULL,
+  `turno` int(11) DEFAULT NULL,
+  `horario` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `disciplina`
 --
 
-INSERT INTO `disciplina` (`id`, `descricao`, `idGrauEnsino`, `idProf`) VALUES
-(1, 'Linguagem de programaÃ§Ã£o', 2, 5),
-(2, 'PortuguÃªs', 1, 2),
-(5, 'Fisica', 1, 2),
-(6, 'Ingles', 2, 4),
-(7, 'PortuguÃªs', 1, 4),
-(8, 'Fisica', 2, 2),
-(9, 'RedaÃ§Ã£o', 1, 4),
-(10, 'ReligiÃ£o', 2, 2),
-(11, 'ReligiÃ£o', 1, 5);
+INSERT INTO `disciplina` (`id`, `descricao`, `idGrauEnsino`, `idProf`, `turno`, `horario`) VALUES
+(1, 'Linguagem de programaÃ§Ã£o', 2, 5, 1, '05:28:29'),
+(2, 'PortuguÃªs', 1, 2, 1, '09:00:00'),
+(5, 'Fisica', 1, 2, 2, '08:12:00'),
+(6, 'Ingles', 2, 4, 2, '09:18:08'),
+(7, 'PortuguÃªs', 2, 2, 1, '07:23:00'),
+(8, 'Fisica', 2, 2, 2, '17:40:00'),
+(9, 'RedaÃ§Ã£o', 1, 4, 1, '00:00:00'),
+(10, 'ReligiÃ£o', 2, 2, 2, '00:00:00'),
+(11, 'ReligiÃ£o', 1, 5, 1, '00:00:00');
 
 -- --------------------------------------------------------
 
@@ -116,7 +121,6 @@ CREATE TABLE `matricula` (
   `id` int(11) NOT NULL,
   `idAluno` int(11) NOT NULL,
   `idDisciplina` int(11) NOT NULL,
-  `idTurno` int(11) NOT NULL,
   `dataMatricula` date NOT NULL,
   `dataConclusao` date DEFAULT NULL,
   `status` set('ATIVO','INATIVO','','') NOT NULL,
@@ -126,8 +130,16 @@ CREATE TABLE `matricula` (
   `nota4` float DEFAULT NULL,
   `nota5` float DEFAULT NULL,
   `media` float DEFAULT NULL,
-  `horaAula` float DEFAULT NULL
+  `horaAula` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `matricula`
+--
+
+INSERT INTO `matricula` (`id`, `idAluno`, `idDisciplina`, `dataMatricula`, `dataConclusao`, `status`, `nota1`, `nota2`, `nota3`, `nota4`, `nota5`, `media`, `horaAula`) VALUES
+(2, 2, 5, '2018-12-27', NULL, 'ATIVO', NULL, NULL, NULL, NULL, NULL, NULL, '08:12:00'),
+(8, 1, 9, '2018-12-28', NULL, 'ATIVO', NULL, NULL, NULL, NULL, NULL, NULL, '00:00:00');
 
 -- --------------------------------------------------------
 
@@ -167,6 +179,14 @@ CREATE TABLE `turno` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Extraindo dados da tabela `turno`
+--
+
+INSERT INTO `turno` (`id`, `descricao`) VALUES
+(1, 'MATUTINO'),
+(2, 'NOTURNO');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -177,14 +197,16 @@ ALTER TABLE `aluno`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `rg` (`rg`),
   ADD UNIQUE KEY `cpf` (`cpf`),
-  ADD UNIQUE KEY `tituloEleitor` (`tituloEleitor`);
+  ADD UNIQUE KEY `tituloEleitor` (`tituloEleitor`),
+  ADD KEY `turno` (`turno`);
 
 --
 -- Indexes for table `disciplina`
 --
 ALTER TABLE `disciplina`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idProf` (`idProf`);
+  ADD KEY `idProf` (`idProf`),
+  ADD KEY `disciplina_ibfk_2` (`turno`);
 
 --
 -- Indexes for table `frequencia`
@@ -200,8 +222,7 @@ ALTER TABLE `frequencia`
 ALTER TABLE `matricula`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idAluno` (`idAluno`),
-  ADD KEY `idDisciplina` (`idDisciplina`),
-  ADD KEY `idTurno` (`idTurno`);
+  ADD KEY `idDisciplina` (`idDisciplina`);
 
 --
 -- Indexes for table `professor`
@@ -221,10 +242,17 @@ ALTER TABLE `turno`
 --
 
 --
+-- Limitadores para a tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `aluno_turnofk_1` FOREIGN KEY (`turno`) REFERENCES `turno` (`id`);
+
+--
 -- Limitadores para a tabela `disciplina`
 --
 ALTER TABLE `disciplina`
-  ADD CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`idProf`) REFERENCES `professor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`idProf`) REFERENCES `professor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disciplina_ibfk_2` FOREIGN KEY (`turno`) REFERENCES `turno` (`id`);
 
 --
 -- Limitadores para a tabela `frequencia`
@@ -238,8 +266,7 @@ ALTER TABLE `frequencia`
 --
 ALTER TABLE `matricula`
   ADD CONSTRAINT `matricula_ibfk_4` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`id`),
-  ADD CONSTRAINT `matricula_ibfk_5` FOREIGN KEY (`idAluno`) REFERENCES `aluno` (`id`),
-  ADD CONSTRAINT `matricula_ibfk_6` FOREIGN KEY (`idTurno`) REFERENCES `turno` (`id`);
+  ADD CONSTRAINT `matricula_ibfk_5` FOREIGN KEY (`idAluno`) REFERENCES `aluno` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
