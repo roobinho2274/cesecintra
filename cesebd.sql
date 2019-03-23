893004e8aba06f08d8846ad9940eb7228f4b1670
jonas braga <jonasbraga2001@gmail.com>
2019-03-23 17:15:24 -0300

    adicionado o novo banco e limpo

---
+++ cesebd.sql
@@ -0,0 +1,190 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 23-Mar-2019 às 20:27
+-- Versão do servidor: 10.1.31-MariaDB
+-- versão do PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cesecbd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno`
+--
+
+CREATE TABLE `aluno` (
+  `id` int(11) NOT NULL,
+  `rg` varchar(13) NOT NULL,
+  `cpf` varchar(16) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `turno` int(11) NOT NULL,
+  `orgaoExpedidor` varchar(10) DEFAULT NULL,
+  `mae` varchar(100) DEFAULT NULL,
+  `pai` varchar(100) DEFAULT NULL,
+  `tituloEleitor` bigint(20) DEFAULT NULL,
+  `reservista` bigint(20) DEFAULT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `estadoCivil` varchar(10) DEFAULT NULL,
+  `logradouro` varchar(100) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `complemento` varchar(100) DEFAULT NULL,
+  `numeroResidencial` varchar(5) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `cep` bigint(20) DEFAULT NULL,
+  `estado` varchar(4) DEFAULT NULL,
+  `telefone` bigint(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `celular` bigint(20) DEFAULT NULL,
+  `status` varchar(8) NOT NULL,
+  `datacadastro` date NOT NULL,
+  `grauensino` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `disciplina`
+--
+
+CREATE TABLE `disciplina` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(100) NOT NULL,
+  `idGrauEnsino` int(11) NOT NULL,
+  `idProf` int(11) NOT NULL,
+  `turno` int(11) DEFAULT NULL,
+  `horario` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `frequencia`
+--
+
+CREATE TABLE `frequencia` (
+  `id` int(11) NOT NULL,
+  `idDisciplina` int(11) NOT NULL,
+  `idAluno` int(11) NOT NULL,
+  `mes` varchar(15) NOT NULL,
+  `ano` year(4) NOT NULL,
+  `horas` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `professor`
+--
+
+CREATE TABLE `professor` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `senha` int(11) NOT NULL,
+  `tipo` set('adm','secretaria','professor','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turno`
+--
+
+CREATE TABLE `turno` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `turno`
+--
+
+INSERT INTO `turno` (`id`, `descricao`) VALUES
+(1, 'MATUTINO'),
+(2, 'NOTURNO');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `rg` (`rg`),
+  ADD KEY `turno` (`turno`);
+
+--
+-- Indexes for table `disciplina`
+--
+ALTER TABLE `disciplina`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idProf` (`idProf`),
+  ADD KEY `disciplina_ibfk_2` (`turno`);
+
+--
+-- Indexes for table `frequencia`
+--
+ALTER TABLE `frequencia`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idAluno` (`idAluno`),
+  ADD KEY `idDisciplina` (`idDisciplina`);
+
+--
+-- Indexes for table `professor`
+--
+ALTER TABLE `professor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- Indexes for table `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `aluno_turnofk_1` FOREIGN KEY (`turno`) REFERENCES `turno` (`id`);
+
+--
+-- Limitadores para a tabela `disciplina`
+--
+ALTER TABLE `disciplina`
+  ADD CONSTRAINT `disciplina_ibfk_1` FOREIGN KEY (`idProf`) REFERENCES `professor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disciplina_ibfk_2` FOREIGN KEY (`turno`) REFERENCES `turno` (`id`);
+
+--
+-- Limitadores para a tabela `frequencia`
+--
+ALTER TABLE `frequencia`
+  ADD CONSTRAINT `frequencia_ibfk_2` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`id`),
+  ADD CONSTRAINT `frequencia_ibfk_3` FOREIGN KEY (`idAluno`) REFERENCES `aluno` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
